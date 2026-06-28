6a0e755aa06d9a36382444535f90570b27bec534
m4x150 <gipertronik1@yandex.ru>
2026-06-28 12:53:08 +0300

    add custom chart

--- superset-frontend/src/visualizations/presets/MainPreset.ts
+++ superset-frontend/src/visualizations/presets/MainPreset.ts
@@ -22,6 +22,7 @@ import {
   Preset,
   VizType,
 } from '@superset-ui/core';
+import BigNumberTrendlineCustomPlugin from '@superset-ui/plugin-chart-big-number-custom' //Кастомный чарт Арины
 import CalendarChartPlugin from '@superset-ui/legacy-plugin-chart-calendar';
 import ChordChartPlugin from '@superset-ui/legacy-plugin-chart-chord';
 import CountryMapChartPlugin from '@superset-ui/legacy-plugin-chart-country-map';
@@ -211,6 +212,7 @@ export default class MainPreset extends Preset {
         }).configure({ key: VizType.Cartodiagram }),
         ...experimentalPlugins,
         ...agGridTablePlugin,
+        new BigNumberTrendlineCustomPlugin().configure({ key: 'big_number_trendline_custom' }), //Кастомный чарт Арины
       ],
     });
   }
